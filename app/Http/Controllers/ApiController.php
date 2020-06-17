3092863adaddd6ef4377eed2dfbacdd44df56096
Karthick <karthiram165@gmail.com>
2020-06-17 13:58:21 +0530

    laravel

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Image;
+
+class ApiController extends Controller
+{
+    public function image(Request $request)
+    {
+        $image = $request->file;
+        $image_new_name = time() . $image->getClientOriginalName();
+        $image->move('image', $image_new_name);
+        $data = new Image;
+        $data->image = $image_new_name;
+        $data->number = $request->number;
+        return response()->json($data);
+    }
+
+    public function getImage($number)
+    {
+        $image = Image::where('number', $number)->first();
+        return response()->json($image);
+    }
+}
